2d3c54af8833602cbc5079bf1d08137e0a4f5997
Noppon kuankumpor <33632176+NPKman@users.noreply.github.com>
2024-05-27 16:48:40 +0700

    Add project files.

---
+++ Zipfile App/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Zipfile_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtSourceFolderPath;
+        private System.Windows.Forms.TextBox txtZipFolderPath;
+        private System.Windows.Forms.TextBox txtStartDate;
+        private System.Windows.Forms.TextBox txtEndDate;
+        private System.Windows.Forms.Button btnCreateZip;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtSourceFolderPath = new System.Windows.Forms.TextBox();
+            this.txtZipFolderPath = new System.Windows.Forms.TextBox();
+            this.txtStartDate = new System.Windows.Forms.TextBox();
+            this.txtEndDate = new System.Windows.Forms.TextBox();
+            this.btnCreateZip = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSourceFolderPath
+            // 
+            this.txtSourceFolderPath.Location = new System.Drawing.Point(169, 119);
+            this.txtSourceFolderPath.Name = "txtSourceFolderPath";
+            this.txtSourceFolderPath.Size = new System.Drawing.Size(200, 22);
+            this.txtSourceFolderPath.TabIndex = 0;
+            // 
+            // txtZipFolderPath
+            // 
+            this.txtZipFolderPath.Location = new System.Drawing.Point(169, 149);
+            this.txtZipFolderPath.Name = "txtZipFolderPath";
+            this.txtZipFolderPath.Size = new System.Drawing.Size(200, 22);
+            this.txtZipFolderPath.TabIndex = 1;
+            // 
+            // txtStartDate
+            // 
+            this.txtStartDate.Location = new System.Drawing.Point(169, 179);
+            this.txtStartDate.Name = "txtStartDate";
+            this.txtStartDate.Size = new System.Drawing.Size(200, 22);
+            this.txtStartDate.TabIndex = 2;
+            this.txtStartDate.Text = "2024-03-01";
+            // 
+            // txtEndDate
+            // 
+            this.txtEndDate.Location = new System.Drawing.Point(169, 209);
+            this.txtEndDate.Name = "txtEndDate";
+            this.txtEndDate.Size = new System.Drawing.Size(200, 22);
+            this.txtEndDate.TabIndex = 3;
+            this.txtEndDate.Text = "2024-03-01";
+            // 
+            // btnCreateZip
+            // 
+            this.btnCreateZip.Location = new System.Drawing.Point(169, 239);
+            this.btnCreateZip.Name = "btnCreateZip";
+            this.btnCreateZip.Size = new System.Drawing.Size(200, 23);
+            this.btnCreateZip.TabIndex = 4;
+            this.btnCreateZip.Text = "สร้าง Zip";
+            this.btnCreateZip.UseVisualStyleBackColor = true;
+            this.btnCreateZip.Click += new System.EventHandler(this.btnCreateZip_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "sourceFolderPath";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "zipFolderPath";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Start Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "End Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.Location = new System.Drawing.Point(162, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(208, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Zip File APP";
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Location = new System.Drawing.Point(47, 297);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtConsole.Size = new System.Drawing.Size(322, 149);
+            this.txtConsole.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Process Bar";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(38, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(119, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(431, 464);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtConsole);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreateZip);
+            this.Controls.Add(this.txtEndDate);
+            this.Controls.Add(this.txtStartDate);
+            this.Controls.Add(this.txtZipFolderPath);
+            this.Controls.Add(this.txtSourceFolderPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "โปรแกรมสร้างไฟล์ Zip";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtConsole;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Zipfile App/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zipfile_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadImage();
+        }
+
+        private void LoadImage()
+        {
+            // กำหนดเส้นทางไปยังไฟล์รูปภาพ
+            pictureBox1.Image = Properties.Resources.fatman; // เปลี่ยนเส้นทางให้ตรงกับไฟล์รูปภาพของคุณ
+
+          
+        }
+        private void btnCreateZip_Click(object sender, EventArgs e)
+        {
+            // อ่านค่าจาก TextBox
+            string sourceFolderPath = txtSourceFolderPath.Text;
+            string zipFolderPath = txtZipFolderPath.Text;
+            string startDateStr = txtStartDate.Text;
+            string endDateStr = txtEndDate.Text;
+
+            // แปลงวันที่จาก string เป็น DateTime
+            DateTime startDate = DateTime.ParseExact(startDateStr, "yyyy-MM-dd", null);
+            DateTime endDate = DateTime.ParseExact(endDateStr, "yyyy-MM-dd", null);
+
+            // ค้นหาไฟล์ในโฟลเดอร์ที่มีวันที่แก้ไขอยู่ในช่วงวันที่ที่กำหนด
+            var filesToZip = Directory.GetFiles(sourceFolderPath)
+                                      .Where(file => File.GetLastWriteTime(file) >= startDate &&
+                                                     File.GetLastWriteTime(file) <= endDate)
+                                      .ToList();
+
+            if (filesToZip.Count == 0)
+            {
+                UpdateConsole($"ไม่พบไฟล์ที่มีวันที่แก้ไขระหว่าง {startDateStr} และ {endDateStr}");
+                return;
+            }
+
+            // จัดกลุ่มไฟล์ตามวันที่แก้ไข
+            var groupedFiles = filesToZip.GroupBy(file => File.GetLastWriteTime(file).Date);
+
+            foreach (var group in groupedFiles)
+            {
+                DateTime date = group.Key;
+                string dateString = date.ToString("yyyy-MM-dd");
+                string zipFileName = $"Files_{dateString}.zip";
+                string zipPath = Path.Combine(zipFolderPath, zipFileName);
+
+                using (var archive = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+                {
+                    foreach (string file in group)
+                    {
+                        archive.CreateEntryFromFile(file, Path.GetFileName(file));
+                        UpdateConsole($"เพิ่มไฟล์: {file}");
+                    }
+                }
+
+                UpdateConsole($"สร้าง zip เรียบร้อย: {zipPath}");
+            }
+
+            UpdateConsole("กระบวนการเสร็จสมบูรณ์");
+        }
+
+        private void UpdateConsole(string message)
+        {
+            txtConsole.AppendText(message + Environment.NewLine);
+        }
+    }
+}
